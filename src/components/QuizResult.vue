a0f57b02758d599777905446c18c1b7f37e34b3f
rhyman18 <rhyman18@gmail.com>
2024-07-18 23:54:57 +0700

    initial push

---
+++ src/components/QuizResult.vue
@@ -0,0 +1,29 @@
+<script setup>
+const { quizQuestionLength, numberOfCorrectAnswers } = defineProps([
+  'quizQuestionLength',
+  'numberOfCorrectAnswers'
+])
+</script>
+
+<template>
+  <section class="results">
+    <p>Score</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quizQuestionLength }}</h1>
+    <RouterLink to="/">Go Back</RouterLink>
+  </section>
+</template>
+
+<style scoped>
+.results {
+  text-align: center;
+  padding: 100px 0;
+}
+
+p {
+  font-size: 25px;
+}
+
+h1 {
+  font-size: 80px;
+}
+</style>
